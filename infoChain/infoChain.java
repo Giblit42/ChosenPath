e55830627a828412deec6fc97d1e0e29852b5cef
Giblit42 <71239011+Giblit42@users.noreply.github.com>
2024-10-25 13:42:28 -0500

    Testing if new branch is choosen

--- infoChain/infoChain.java
+++ infoChain/infoChain.java
@@ -29,33 +29,51 @@ public class infoChain {
 		Thread.sleep(3000);						//begin to delay the story
 
 		//Begin the story
+
+		//Start asking the player story questions here
+		System.out.println("You awake in a dark room to your left is an alarm clock on a night stand\n");
+		Thread.sleep(3000);
+		System.out.println("To your right is a tall lamp that is currently turned off\n");
+		Thread.sleep(3000);
+		System.out.println("At your feet is a hairless overweight cat named Tibs\n");
+		Thread.sleep(3000);
+
+		//fist choice question
+		System.out.println("You ccan\n");
+
+		// first set of choices
+		System.out.println("A: Turn the alarm clock so you can see the time\n");
+		Thread.sleep(2500);					// slow down options to give ample time for the user to read options
+		System.out.println("B: Turn on your lamp\n");
+		Thread.sleep(2500);					// slow down options to give ample time for the user to read options
+		System.out.println("C: Attemp to toss Tibs off of you bed\n");
+		Thread.sleep(2500);					// slow down options to give ample time for the user to read options
+
+		System.out.println("Choose one\n");
 		playerTurn(myObj);
+		response = myObj.nextLine();
+
+		//Take new path
+		if(response.equals("A") || response.equals("a")){
+			System.out.println("You turn the alram clock to see what time it is,and see that you have 3 more hours until you go into work\n");
+				Thread.sleep(2500);				// slow down options to give ample time for the user to read options
+		}
+		
+		if(response.equals("B") || response.equals("b")) {	//user choose b
+			System.out.println("You move closer to the foot of the bed and begin to stroke the fuzzy fat ball until you hear him begin to purr.\n");
+
+			Thread.sleep(2500);			// slow down options to give ample time for the user to read options
+		}
+
+		if(response.equals("C") || response.equals("c")){
+			System.out.println("You close your eyes until you drift back off to sleep");
+			Thread.sleep(2500);
+		}
 	}
 
-	private static void playerTurn(Scanner scanner)throws InterruptedException{
+	private static void playerTurn(Scanner scanner){
         String userInput;
         while(true){
-
-			//Start asking the player story questions here
-            System.out.println("You awake in a dark room to your left is an alarm clock on a night stand\n");
-			Thread.sleep(3000);
-			System.out.println("To your right is a tall lamp that is currently turned off\n");
-			Thread.sleep(3000);
-			System.out.println("At your feet is a hairless overweight cat named Tibs\n");
-			Thread.sleep(3000);
-
-			//fist choice question
-			System.out.println("You ccan\n");
-
-			// first set of choices
-			System.out.println("A: Turn the alarm clock so you can see the time\n");
-			Thread.sleep(2500);					// slow down options to give ample time for the user to read options
-			System.out.println("B: Turn on your lamp\n");
-			Thread.sleep(2500);					// slow down options to give ample time for the user to read options
-			System.out.println("C: Attemp to toss Tibs off of you bed\n");
-			Thread.sleep(2500);					// slow down options to give ample time for the user to read options
-
-			System.out.println("Choose one\n");
 
             userInput = scanner.nextLine();
             if(isValid(userInput)){
